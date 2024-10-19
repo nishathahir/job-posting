bf2d4903d745f0d7064bc8aafdc10c1768deb858
nisha-apiplatform <nisha@apiplatform.io>
2024-10-19 20:12:54 +0530

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 // src/App.tsx
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import Interview from "./components/Interview";
-import Navbar from "./components/Navbar";
 
 // import Interview from "./pages/Interview"; // Example page
 // import Register from "./pages/Register";   // Example page
